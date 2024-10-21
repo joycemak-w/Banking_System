962d4bdbf2f37dea4c3160e78978aaaeed41360c
Joyce Mak <joycemakwaihang128@gmail.com>
2024-10-21 09:15:03 +0800

    alert show relative stock SMA

--- reports/alerts.py
+++ reports/alerts.py
@@ -58,19 +58,23 @@ def rsi_over(symbol_option):
 
     rsi = 100 * avg_up / (avg_up + avg_down)
     rsi = rsi.sort_index()
-        # Initialize lists to store overbought and oversold dates
-    overbought_dates = []
-    oversold_dates = []
+     # Initialize lists to store overbought and oversold dates
+    # overbought_dates = []
+    # oversold_dates = []
+    over_dates = []
 
     # Loop through the RSI DataFrame to check conditions
     for index, row in rsi.items():
-        # st.write(index)
-        if row > 70:
-            overbought_dates.append(datetime.fromisoformat(str(index)).strftime('%Y-%m-%d'))
-        elif row < 30:
-            oversold_dates.append(datetime.fromisoformat(str(index)).strftime('%Y-%m-%d'))
-
-    return overbought_dates, oversold_dates
+        if datetime.fromisoformat(str(index)).date() >= date.today() - timedelta(days=30):
+            if row > 70:
+                over_dates.append((datetime.fromisoformat(str(index)).date(),'overbought'))
+            if row < 30:
+                over_dates.append((datetime.fromisoformat(str(index)).date(),'oversold'))
+    if over_dates != []:
+        return over_dates[-1]
+    else:
+        pass
+    # return over_dates
 
 # def correlation(symbol_options):
 #     hsi_df = pd.read_csv('hang_seng_index.csv')
@@ -107,7 +111,7 @@ def rsi_over(symbol_option):
 
 #     return strong_positive_correlation, strong_negative_correlation
 
-def correlation(symbol_options):
+def correlation_a(symbol_options):
     hsi_df = pd.read_csv('hang_seng_index.csv')
     hsi_sm = hsi_df['Symbol'].to_list()
 
@@ -120,7 +124,7 @@ def correlation(symbol_options):
     # Fetch historical stock data, ignoring stocks that fail to download
     for symbol in hsi_sm:
         try:
-            stock_data = yf.download(symbol, period='1mo')['Adj Close']
+            stock_data = yf.download(symbol, period='2y')['Adj Close']
             if not stock_data.empty:
                 hsi_data[symbol] = stock_data
         except Exception as e:
@@ -140,21 +144,118 @@ def correlation(symbol_options):
     return strong_correlation
 
 symbol_list = ['0388.HK', '0700.HK']
+conn = sqlite3.connect('banking_system.db')
+c = conn.cursor()
+c.execute("SELECT * FROM users WHERE username = ?", (st.session_state.user,))
+user_info = c.fetchone()
+query = """
+SELECT DISTINCT r.symbol, r.date
+FROM stock_record r
+JOIN (
+    SELECT symbol, MAX(date) AS latest_date
+    FROM stock_record
+    WHERE user_id = ?
+    GROUP BY symbol
+) AS latest_records ON r.symbol = latest_records.symbol AND r.date = latest_records.latest_date
+WHERE r.user_id = ? ORDER BY r.date desc
+"""
+df_user_symbol = pd.read_sql(query, conn, params=(user_info[0],user_info[0]))
+sm = pd.read_csv('hk_tickers.csv')
+# st.write(df_user_symbol)
+symbol_list = df_user_symbol['symbol']
+name_list = {}
+for s in symbol_list:
+    name_list[s] = sm[sm['Symbol']==s]['Name']
+# st.write(name_list['2318.HK'].iloc[0])
+# for s in df_user_symbol['symbol']:
+#     symbol_list.append(s)
 with st.spinner(text="Loading the notifications..."):
     # p, n = correlation(symbol_list)
     # st.write(p.get('0388.HK', 'No strong positive correlations'))
     # st.write(n.get('0388.HK', 'No strong negative correlations'))
-    correlation_results = correlation(symbol_list)
+    correlation_results = correlation_a(symbol_list)
     for symbol_option, correlations in correlation_results.items():
-        # st.write(symbol_option)
         # st.write(correlations)
-        st.write(sma_cross(symbol_option))
-        # st.write(rsi_over(symbol_option))
-        st.subheader("P")
-        for correlation in correlations['positive']:
-            
-            st.write(sma_cross(correlation))
-        st.subheader("N")
-        for correlation in correlations['negative']:
-            
-            st.write(sma_cross(correlation))
+        sma_user_stock = sma_cross(symbol_option)
+        rsi_user_stock = rsi_over(symbol_option)
+        if sma_user_stock is not None or rsi_user_stock is not None:
+            st.subheader(f"{name_list[symbol_option].iloc[0]} ({symbol_option})")
+            if sma_user_stock is not None:
+                if sma_user_stock[2] == 'up':
+                    st.write(f":green[**{symbol_option}** may increase] as there is a golden cross on {datetime.fromisoformat(str(sma_user_stock[0])).date()}.")
+                elif sma_user_stock[2] == 'down':
+                    st.write(f":red[**{symbol_option}** may decrease] as there is a death cross on {datetime.fromisoformat(str(sma_user_stock[0])).date()}.")
+            if rsi_user_stock is not None:
+                if rsi_user_stock[1] == 'overbought':
+                    st.write(f":red[**{symbol_option}** may decrease] as there is overbought on {rsi_user_stock[0]}.")
+                elif rsi_user_stock[1] == 'oversold':
+                    st.write(f":green[**{symbol_option}** may decrease] as there is oversold on {rsi_user_stock[0]}.")
+            # st.write(sma_user_stock)
+            # st.write(rsi_user_stock)
+
+            in_de = []
+            for correlation in correlations['positive']:
+                sma = sma_cross(correlation)
+                rsi = rsi_over(correlation)
+                if sma is not None:
+                    if sma[2] == 'up':
+                        in_de.append('in')
+                    elif sma[2] == 'down':
+                        in_de.append('de')
+
+            for correlation in correlations['negative']:
+                sma = sma_cross(correlation)
+                rsi = rsi_over(correlation)
+                if sma is not None:
+                    if sma[2] == 'down':
+                        in_de.append('in')
+                    elif sma[2] == 'up':
+                        in_de.append('de')
+
+            # Determine summary message based on in_de
+            if in_de:
+                if in_de.count('in') / len(in_de) >= 0.7:
+                    summary = "Overall related stocks indicate an :green[INCREASE]."
+                elif in_de.count('de') / len(in_de) >= 0.7:
+                    summary = "Overall related stocks indicate a :red[DECREASE]."
+                else:
+                    summary = "Low similarity among related stocks."
+                with st.expander(summary):
+                    # st.write("Positive")
+                    for correlation in correlations['positive']:
+                        sma  = sma_cross(correlation)
+                        rsi = rsi_over(correlation)
+                        if sma is not None:
+                            # st.write(correlation)
+                            # st.write(sma)
+                            st.write(f"**{symbol_option}** have a positive correlation with **{correlation}**.")
+                            if sma[2] == 'up':
+                                st.write(f":green[**{symbol_option}** may increase] as there is a golden cross on {correlation} on {datetime.fromisoformat(str(sma[0])).date() }.")
+                            elif sma[2] == 'down':
+                                st.write(f":red[**{symbol_option}** may decrease] as there is a death cross on **{correlation}** on {datetime.fromisoformat(str(sma[0])).date() }.")
+                            st.markdown('----')
+                    # st.write("Negative")
+                    for correlation in correlations['negative']:
+                        sma  = sma_cross(correlation)
+                        rsi = rsi_over(correlation)
+                        if sma is not None:
+                            # st.write(correlation)
+                            # st.write(sma)
+                            st.write(f"**{symbol_option}** have a negative correlation with **{correlation}**.")
+                            if sma[2] == 'down':
+                                st.write(f":green[**{symbol_option}** may increase] due there is death cross on {correlation} on {datetime.fromisoformat(str(sma[0])).date() }.")
+                            elif sma[2] == 'up':
+                                st.write(f":red[**{symbol_option}** may decrease] due there is golden cross on **{correlation}** on {datetime.fromisoformat(str(sma[0])).date() }.")
+                            st.markdown('----')
+                
+                # summary = ""
+                # if in_de.count('in')/ len(in_de) >= 0.7:
+                #     # st.write(in_de.count('in')/ len(in_de))
+                #     summary = "Overall related stocks cause increasement."
+                # elif in_de.count('de')/ len(in_de) >= 0.7:
+                #     summary = "Overall related stocks cause decreasement."
+                # else:
+                #     summary = "Low similarity among related stocks"
+                # st.write(summary)
+            st.markdown('----')
+
